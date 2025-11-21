50e23485ded312035ad4eace806a3292fc9aebcf
Oparaiwu Silas <silas@pixelsgroup.ng>
2025-11-21 12:42:53 +0100

    Initial commit3

--- ViewModels/Register.cs
+++ ViewModels/Register.cs
@@ -4,8 +4,6 @@
     {
         public string? email { get; set; }
         public string? password { get; set; }
-        public string? memberId {get; set;}
-        public string? status {get; set;}
         
     }
 }
